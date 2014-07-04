301dda91b295e80299a5d3cbc9e08fc8c9964938
Alp Mestanogullari <alpmestan@gmail.com>
2014-07-04 14:12:36 +0200

    Improved my travis-fu. It's now a more sensible (idiomatic) travis script that tries to build with several GHC versions.

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ cabal sandbox add-source servant/
 cabal sandbox add-source servant-pool/
 cabal sandbox add-source servant-postgresql/
 cabal sandbox add-source servant-scotty/
+cabal install servant servant-pool servant-postgresql servant-scotty --only-d
