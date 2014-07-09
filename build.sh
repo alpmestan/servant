73ac2f62cc5911455c8942d07a8b7cfd2b33a9cb
Alp Mestanogullari <alpmestan@gmail.com>
2014-07-09 15:27:39 +0200

    update init/build scripts to include servant-response

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cabal install servant servant-pool servant-postgresql servant-scotty servant-example --reinstall
+cabal install servant servant-pool servant-response servant-postgresql servant-scotty servant-example
--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@ cabal sandbox delete
 cabal sandbox init
 cabal sandbox add-source servant/
 cabal sandbox add-source servant-example/
+cabal sandbox add-source servant-response/
 cabal sandbox add-source servant-pool/
 cabal sandbox add-source servant-postgresql/
 cabal sandbox add-source servant-scotty/
-cabal install servant servant-pool servant-postgresql servant-scotty servant-example --only-d
+cabal install servant servant-pool servant-response servant-postgresql servant-scotty servant-example --only-d
