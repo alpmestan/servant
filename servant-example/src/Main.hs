5d6ea15d3a96f4ad63ca1a93860f953b7badfa01
Alp Mestanogullari <alpmestan@gmail.com>
2014-07-19 03:44:20 +0200

    document Main.main

--- servant-example/src/Main.hs
+++ servant-example/src/Main.hs
@@ -13,11 +13,30 @@ import Model.User
 
 main :: IO ()
 main = do
+  -- we initialize our connection pool context
   ctx <- getContext
+
+  -- we run a web application that listens on
+  -- port 4321
   scottyT 4321 id id $ do
+    -- a default error handler that sends
+    -- status code 400 along with the ServiceError
+    -- value as JSON
     defaultHandler $ \err -> do
       status badRequest400
       json err
+
+    -- magic: we turn our Resource description into
+    -- three endpoints:
+
+    -- * GET /users will list all users in JSON.
+    -- * POST /users will try to decode a user in JSON format
+    --   from the request body and add it to the database, returning
+    --   some useful response in JSON along with the appropriate status
+    --   depending on whether the user was successfully added.
+    -- * DELETE /users/alp@blah.com will delete (if it exists) the user
+    --   with this email from the database, responding in JSON with some
+    --   information on how things went, just like in the previous endpoint.
     runResource $ users ctx
 
 users :: Context Connection
